702937231e94e9177fc8f60137eb1d957320ecea
gracerabano <gracearabano@gmail.com>
2023-02-23 16:57:15 -0600

    quote form roughly done

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import {fuel_quote_form} from './fuel_quote_form'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
