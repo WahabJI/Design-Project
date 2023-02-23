7351b1bda59349f80e447961ad2a7e816c8fd556
Wahab Javed <wahab.javed@live.com>
2023-02-23 00:33:48 -0600

    finished original login page

---
+++ pages/login_page.js
@@ -0,0 +1,62 @@
+
+export default function login_page() {
+    return (
+      <div className="bg-grey">
+        {/* TOP BAR */}
+        <nav className="flex absolute w-full items-center font-bold text-4xl text-beige bg-light_blue h-14">
+          <div className="ml-4">
+            FUEL QUOTER
+          </div>
+          <ul className="ml-auto left-0 right-0 top-full inline-flex">
+            <li className="flex mr-4 items-center">
+              <span>HOME</span>
+            </li>
+            <li className="flex mr-4 items-center">
+              <span>REGISTER</span>
+            </li>
+          </ul>
+        </nav>
+
+
+        {/* LOGIN FORM */}
+        <div className="flex h-screen bg-gray-100">
+          <div className="px-8 py-6 text-left bg-white shadow-lg m-auto">
+            <h3 className="text-2xl font-bold text-center">Login to your account</h3>
+            <form action="">
+                <div className="mt-4">
+                  <div>
+                    <label className="block" for="email">Email</label>
+                    <input type="text" placeholder="Email" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
+                  </div>
+                  <div className="mt-4">
+                      <label className="block">Password</label>
+                      <input type="password" placeholder="Password" className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"/>
+                  </div>
+                  <div className="flex items-baseline justify-between">
+                      <button className="px-6 py-2 mt-4 text-black bg-light_blue rounded-lg hover:bg-light_blue/75 hover:text-beige">Login</button>
+                      <a href="#" className="text-sm text-black hover:underline">Forgot password?</a>
+                  </div>        
+                </div>
+    
+              </form>    
+          </div>
+        </div>
+
+
+        {/* FOOTER */}
+        <div className="flex absolute bottom-0 justify-center text-center align-center h-10 w-full bg-white border-t-2 border-dark_grey pt-1.5">
+          <span><span className="text-light_blue">Created by </span> Mayssam Kalajo, Grace Rabano, Christian Ayala, and Wahab Javed</span>
+        </div>
+
+
+
+      <div className="h-screen bg-light_blue">
+
+      </div>
+
+      </div>
+      
+
+
+    );
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",  
   ],
   theme: {
-    extend: {},
+    colors: {
+      'light_blue':'#3CBAB2',
+      'beige': '#FFFFE3',
+      'grey': '#F5F5F5',
+      'white': '#FFFFFF',
+      'black': '#000000',
+      'red': '#FF0000',
+      'green': '#00FF00',
+      'blue': '#0000FF',
+      'dark_grey': '#DEDEDE',
+    },
+    fontFamily: {
+      
+    },
   },
   plugins: [],
 }
