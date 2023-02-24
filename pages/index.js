eb25aac9364948bd077618d003b1a0da0d6120b5
Wahab Javed <wahab.javed@live.com>
2023-02-24 11:48:08 -0600

    Changed fIle names

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import {login_page} from './login_page'
+import {LoginPage} from './LoginPage'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
