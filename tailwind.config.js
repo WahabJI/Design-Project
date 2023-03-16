4a52aaf3d10644f703d07a6ce66f945850db0314
gracerabano <gracearabano@gmail.com>
2023-03-15 22:54:20 -0500

    Added Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -7,20 +9,24 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",  
   ],
   theme: {
-    colors: {
-      'light_blue':'#3CBAB2',
-      'beige': '#FFFFE3',
-      'grey': '#F5F5F5',
-      'white': '#FFFFFF',
-      'black': '#28484F',
-      'red': '#FF0000',
-      'green': '#00FF00',
-      'blue': '#0000FF',
-      'dark_grey': '#DEDEDE',
-    },
-    fontFamily: {
-      
-    },
+    //  WHY WONT THIS WORK
+    // fontFamily: {
+    //   barlow: ['Barlow', 'sans-serif'],
+    // },
+
+    extend: {
+      colors: {
+        'light_blue':'#3CBAB2',
+        'beige': '#FFFFE3',
+        'grey': '#F5F5F5',
+        'white': '#FFFFFF',
+        'black': '#28484F',
+        'red': '#FF0000',
+        'green': '#00FF00',
+        'blue': '#0000FF',
+        'dark_grey': '#DEDEDE',
+      },
+    }
   },
   plugins: [],
 }
