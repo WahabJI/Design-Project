5caba50e35997a661f825ac3f33019825862eec9
Wahab Javed <wahab.javed@live.com>
2023-03-24 18:22:15 -0500

    added test cases to account for onChange hooks

--- pages/ProfilePage.test.js
+++ pages/ProfilePage.test.js
@@ -1,6 +1,5 @@
 import ProfilePage from "./ProfilePage.js";
-import { render, screen, waitFor } from '@testing-library/react';
-import { useRouter } from "next/router";
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import React from 'react';
 import fetchMock from "jest-fetch-mock";
 fetchMock.enableMocks();
@@ -29,5 +28,42 @@ describe('ProfilePage', () => {
         }));
         render(<ProfilePage/>);
         await waitFor(() => screen.getByText('Profile'));
+    })
+
+    //add a test case to test all the onChange functions
+    it('Should test all the onChange to be working and expect values to be changed and grabbed', async () => {
+        //dummy to just mock the fetch or else the page will not render
+        fetch.mockResponseOnce(JSON.stringify({
+            firstName: "Joe", lastName: "Shmoe", address1: "5098 Jacksonville Rd",
+            address2: "Apartment 1960", city: "Houston", state: "TX", zipCode: "77034"
+        }));
+        const { getByPlaceholderText } = render(<ProfilePage />);
+        const firstNameInput = getByPlaceholderText("First Name");
+        const lastNameInput = getByPlaceholderText("Last Name");
+        const address1Input = getByPlaceholderText("Address 1");
+        const address2Input = getByPlaceholderText("Address 2");
+        const cityInput = getByPlaceholderText("City");
+        const stateInput = getByPlaceholderText("State");
+        const zipInput = getByPlaceholderText("Zip Code");
+
+
+        //simulate typing in the input for all onChange functions
+        fireEvent.change(firstNameInput, { target: { value: 'Joe' } });
+        fireEvent.change(lastNameInput, { target: { value: 'Shmoe' } });
+        fireEvent.change(address1Input, { target: { value: '5098 Jacksonville Rd' } });
+        fireEvent.change(address2Input, { target: { value: 'Apartment 1960' } });
+        fireEvent.change(cityInput, { target: { value: 'Houston' } });
+        fireEvent.change(stateInput, { target: { value: 'TX' } });
+        fireEvent.change(zipInput, { target: { value: '77034' } });
+
+        //expect the values to be changed with what we said above
+        await waitFor(() => expect(firstNameInput.value).toBe('Joe'));
+        await waitFor(() => expect(lastNameInput.value).toBe('Shmoe'));
+        await waitFor(() => expect(address1Input.value).toBe('5098 Jacksonville Rd'));
+        await waitFor(() => expect(address2Input.value).toBe('Apartment 1960'));
+        await waitFor(() => expect(cityInput.value).toBe('Houston'));
+        await waitFor(() => expect(stateInput.value).toBe('TX'));
+        await waitFor(() => expect(zipInput.value).toBe('77034'));
+
     })
 })
