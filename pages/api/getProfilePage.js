a421d100fd8e3541f51d2865189941acf53587a5
Wahab Javed <wahab.javed@live.com>
2023-03-25 00:27:32 -0500

    Added more validation

--- pages/api/getProfilePage.js
+++ pages/api/getProfilePage.js
@@ -18,7 +18,11 @@ export default function getProfilePage(req, res) {
         // but for now we'll just log the data to the console to see the changes as the data in the backend is not persistent
         if(req.body.firstName == null || req.body.lastName == null || req.body.address1 == null || req.body.address2 == null || req.body.city == null || req.body.state == null || req.body.zipCode == null || req.body.firstName.length == 0 || req.body.lastName.length == 0 || req.body.address1.length == 0 || req.body.address2.length == 0 || req.body.city.length == 0 || req.body.state.length == 0 || req.body.zipCode.length == 0){
             throw new Error("Please fill out all fields")
-        } 
+        }
+        
+        if(req.body.firstName.length > 20 || req.body.lastName.length > 20 || req.body.address1.length > 50 || req.body.address2.length > 50 || req.body.city.length > 20 || req.body.state.length > 20 || req.body.zipCode.length > 10){
+            throw new Error("Please enter valid data")
+        }
 
         console.log(req.body)
         res.status(200).json({message: "Profile Updated"})
