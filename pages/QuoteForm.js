320299c698c439d129a1fe978302e4ee7509b00d
gracerabano <gracearabano@gmail.com>
2023-03-27 23:35:52 -0500

    revamp quote page, include price/gal

--- pages/QuoteForm.js
+++ pages/QuoteForm.js
@@ -65,76 +65,92 @@ export default function fuel_quote_form() {
         <main className="flex-grow flex items-center justify-center">
         {/* QUOTE FORM */}
             <div className="flex justify-center items-center">
-                <div className="flex bg-white shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl flex-col">
+                <div className="flex bg-white shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-[80%] flex-col">
                     {/* Title */}
-                    <div className="flex items-center justify-center w-full py-2">
-                        <h1 className="font-bold text-2xl mt-4">Get a Fuel Quote!</h1>
+                    <div className="flex-col flex w-full py-2.5 px-4 justify-center items-center">
+                        <div>
+                            <h1 className="font-bold text-2xl mt-4 mb-2 text-center">Get a Fuel Quote!</h1>
+                        </div>
+                        <div>
+                            <span className='text-center'>Get a quote by checking your info, filling out the rest of the form, and click 
+                            <span className="text-light_blue"> Get Quote </span> below to see your suggested quote on the right. </span>
+                        </div>
                     </div>
                     <hr className="border-black mt-2 mx-16"></hr>
 
                     <div className="flex w-full justify-center px-4 py-6">    
                         {/* left */}
-                        <div className="w-full px-4 lg:w-3/5">
-                            
+                        <div className="w-full px-4 lg:w-1/2 border-r border-dark_grey">
                             <div>
                                 <label className="block font-bold">Address 1</label>
-                                <input disabled type="text" value={userData.address1} placeholder="Address 1" className="w-full px-4 py-2 mt-1 border rounded-md" />
+                                <input disabled type="text" value={userData.address1} placeholder="Address 1" className="w-full px-4 py-2.5 mt-1 border rounded-md" />
                             </div>
                             <div className="mt-4">
                                 <label className="block font-bold mt-4">Address 2</label>
-                                <input disabled type="text" value={userData.address2} placeholder="Address 2 (optional)" className="w-full px-4 py-2 mt-1 border rounded-md" />
+                                <input disabled type="text" value={userData.address2} placeholder="Address 2 (optional)" className="w-full px-4 py-2.5 mt-1 border rounded-md" />
                             </div>
                             <div className="mt-4">
                                 <div className="flex flex-col sm:flex-row">
                                     <div className="relative flex-initial sm:w-1/2">
                                         <label className="block font-bold">City</label>
-                                        <input disabled type="text" value={userData.city} placeholder="City" name="billing-address" className="w-full px-4 py-2 mt-1 border rounded-md" />
+                                        <input disabled type="text" value={userData.city} placeholder="City" name="billing-address" className="w-full px-4 py-2.5 mt-1 border rounded-md" />
                                     </div>
                                     <div className="relative flex-initial sm:w-1/4 mx-1">
                                         <label className="block font-bold">State</label>
-                                        <input disabled type="text" value={userData.state} placeholder="State" name="billing-state" className="w-full px-4 py-2 mt-1 border rounded-md"/>
+                                        <input disabled type="text" value={userData.state} placeholder="State" name="billing-state" className="w-full px-4 py-2.5 mt-1 border rounded-md"/>
                                     </div>
                                     <div className="relative flex-initial sm:w-1/4">
                                         <label className="block font-bold ">Zip</label>
-                                        <input disabled type="text" value={userData.zipCode} placeholder="ZIP" name="billing-zip" className="w-full px-4 py-2 mt-1 border rounded-md" />
+                                        <input disabled type="text" value={userData.zipCode} placeholder="ZIP" name="billing-zip" className="w-full px-4 py-2.5 mt-1 border rounded-md" />
                                     </div>
                                 </div>
-                                <span className="text-sm mt-1 justify-center"> 
+                                <span className="text-sm mt-2 justify-center"> 
                                 Not the right address? Change it on your profile <Link href="/ProfilePage" className="text-light_blue underline hover:font-bold">here</Link>!
                                 </span>
                             </div>
                         </div>
 
-                        {/* right */}
-                        <div className="w-full px-4 lg:w-2/5">
+                        {/* middle */}
+                        <div className="w-full px-4 lg:w-1/4 justify-center">
                         <form onSubmit={handleSubmit}>
                             <div className='mb-4'>
                                 <label className='"block font-bold'>Delivery Date</label>
-                                <input type="date" value={deliveryDate} onChange={(e) => setDeliveryDate(e.target.value)} className="w-full px-4 py-2 mt-1 border rounded-md" required/>
+                                <input type="date" value={deliveryDate} onChange={(e) => setDeliveryDate(e.target.value)} className="w-full px-4 py-2.5 mt-1 border rounded-md" required/>
 
                                 <label className="mt-4 block font-bold">Gallons Requesting</label>
-                                <input type="number" value={gallonsRequested} onChange={(e) => setGallonsRequested(e.target.value)} step="0.01" placeholder="0.00" className="w-full px-4 py-2 mt-1 border rounded-md" required/>
+                                <input type="number" value={gallonsRequested} onChange={(e) => setGallonsRequested(e.target.value)} step="0.01" placeholder="0.00" className="w-full px-4 py-2.5 mt-1 border rounded-md" required/>
                             </div>
                         
-                            <button type="submit" className="block w-1/2 py-2 mt-4 mx-auto text-light_blue border border-light_blue rounded-lg hover:outline-double">
+                            <button type="submit" className="block w-1/2 py-2.5 mt-4 mx-auto text-light_blue border border-light_blue rounded-lg hover:outline-double">
                                 Get Quote
                             </button>
-                        </form>
+                        </form>         
+                        </div>
 
+                        {/* right */}
+                        <div className="w-full px-4 lg:w-1/4 justify-center">
                             {/* will enable this text box once user has requested a quote */}
-                            <div className="mt-4 py-2">
-                                <label className="block font-bold">Suggested Quote</label>
+                        <form>
+                            <div className="mb-4">
+                                <label className="block font-bold">Price Per Gallon</label>
                                 <div className="flex">
-                                    <span className="inline-flex items-center px-4 py-2 mt-1 border-r-none border rounded-l-md"> $ </span>
-                                    <input id="quotePrice" type="number" placeholder="0.00" className="w-full px-4 py-2 mt-1 border rounded-r-md" disabled/>
+                                    <span className="inline-flex items-center px-4 py-2.5 mt-1 border-r-none border rounded-l-md"> $ </span>
+                                    <input id="pricePerGallon" type="number" placeholder="0.00" className="w-full px-4 py-2.5 mt-1 border rounded-r-md" disabled/>
                                 </div>
                             </div>
-                            <button id="quotePriceButton" className="block w-1/2 py-2 mt-2 mb-4 mx-auto bg-light_blue rounded-lg text-beige hover:bg-light_blue/75 hover:text-beige" disabled>
+                            <div className='mt-4'>
+                                <label className="block mt-4 font-bold">Suggested Quote</label>
+                                <div className="flex">
+                                    <span className="inline-flex items-center px-4 py-2.5 mt-1 border-r-none border rounded-l-md"> $ </span>
+                                    <input id="quotePrice" type="number" placeholder="0.00" className="w-full px-4 py-2.5 mt-1 border rounded-r-md" disabled/>
+                                </div>
+                            </div>
+                            <button id="quotePriceButton" className="block w-1/2 py-2.5 mt-4 mx-auto bg-light_blue rounded-lg text-beige hover:bg-light_blue/75 hover:text-beige" disabled>
                                 Order Now
                             </button>
-                            
-                                
+                        </form>          
                         </div>
+
                     </div>
 
 
