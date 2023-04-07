5191d4a2a6c8ba71606adb92d136dc5f7f4bf4f9
gracerabano <gracearabano@gmail.com>
2023-04-07 14:04:39 -0500

    loggedin homepage almost done

--- pages/LoggedInHomePage.jsx
+++ pages/LoggedInHomePage.jsx
@@ -1,157 +1,158 @@
-import Link from 'next/link';
-import Head from 'next/head';
-import Footer from '../components/Footer'
-import localFont from "next/font/local"
-import {useState, useEffect} from 'react'
-import { useSession, signOut } from 'next-auth/react'
-import router from 'next/router'
+import Link from "next/link";
+import Head from "next/head";
+import Footer from "../components/Footer";
+import localFont from "next/font/local";
+import { useState, useEffect } from "react";
+import { useSession, signOut } from "next-auth/react";
+import router from "next/router";
 const barlow = localFont({
   src: "../public/fonts/Barlow-Regular.ttf",
-  weight: '200'
-})
+  weight: "200",
+});
 
 export default function HomePage() {
-    const {data: session, status} = useSession();
-    const [quoteHistory, setQuoteHistory] = useState([]);
-    const [userData, setUserData] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:3000/api/getQuoteHistory')
-        .then(res => res.json())
-        .then(data => {
-            setQuoteHistory(data[0]);
-        })
-    }, [])
+  const { data: session, status } = useSession();
+  const [quoteHistory, setQuoteHistory] = useState([]);
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/getQuoteHistory")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuoteHistory(data[0]);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch('http://localhost:3000/api/getProfilePage', {
-            method: "GET"
-            })
-            .then(res => res.json())
-            .then(data => {
-                setUserData(data);
-            })
-    }, [])
-    
-    if(typeof window !== "undefined" && status === "unauthenticated") {
-      router.push("/LoginPage")
-      return;
-    }
-    return (
-      <div className={barlow.className}>
-        <Head>
-          <title>Fuel Quoter: Get Your Fuel Fast</title>
-          <link rel="icon" href="/gasPump.ico" />
-        </Head>
-        <div className= "flex flex-col h-screen justify-between bg-gray-100">
+  useEffect(() => {
+    fetch("http://localhost:3000/api/getProfilePage", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setUserData(data);
+      });
+  }, []);
+
+  if (typeof window !== "undefined" && status === "unauthenticated") {
+    router.push("/LoginPage");
+    return;
+  }
+  return (
+    <div className={barlow.className}>
+      <Head>
+        <title>Fuel Quoter: Get Your Fuel Fast</title>
+        <link rel="icon" href="/gasPump.ico" />
+      </Head>
+      <div className="flex flex-col h-screen justify-between bg-gray-100">
         {/* TOP BAR */}
         <header>
-          <nav className="relative flex w-full items-center font-bold text-4xl text-beige bg-light_blue shadow-md h-14">
-            <div className="ml-6">
+          <nav className="relative flex w-full items-center font-bold text-4xl text-beige bg-light_blue shadow-md h-16">
+            <div className="ml-8">
               <Link href="/"> FUEL QUOTER </Link>
             </div>
             <ul className="ml-auto left-0 right-0 top-full inline-flex">
-              <li className="flex mr-6 items-center hover:underline">
+              <li className="flex mr-8 items-center hover:underline">
                 <Link href="/QuoteForm"> QUOTE </Link>
               </li>
-              <li className="flex mr-6 items-center hover:underline">
+              <li className="flex mr-8 items-center hover:underline">
                 <Link href="/QuoteHistory"> HISTORY </Link>
               </li>
-              <li className="flex mr-6 items-center hover:underline">
+              <li className="flex mr-8 items-center hover:underline">
                 <Link href="/ProfilePage"> PROFILE </Link>
               </li>
-              <li className="flex mr-6 items-center hover:underline">
+              <li className="flex mr-8 items-center hover:underline">
                 <button onClick={signOut}> LOGOUT </button>
               </li>
             </ul>
           </nav>
         </header>
 
-
-        {/* LOGIN FORM */}
-        <div className="flex flex-col py-16 justify-between h-screen bg-gray-100">
-          <div className="px-8 py-6 text-left w-5/6 bg-white shadow-lg m-auto">
-            <h3 className="text-2xl font-bold">YOUR LAST QUOTE</h3>
-            <div className="flex flex-row">
-              <div>
-                <div className="mt-4 flex flex-row space-x-2">
-                    <div className="text-light_blue">Gallons Requested:</div>
-                    <div className="text-black inline-block">{quoteHistory.gallonsRequested || "$ "+(0).toFixed(2)}</div>
-                  </div>
-                  <div className="flex flex-row space-x-11">
-                  <div className="text-light_blue">Price / Gallon:</div>
-                  <div className="text-black inline-block">{quoteHistory.pricePerGallon || "$ "+(0).toFixed(2)}</div>
-                </div>
-                  <div className="flex flex-row space-x-12">
-                    <div className="text-light_blue">Amount Paid:</div>
-                    <div className="text-black inline-block">{quoteHistory.totalAmountDue || "$ "+(0).toFixed(2)}</div>
-                  </div>
-                  
-              </div>
-              <div className="pl-24 pr-10">
-                <div className="mt-4 flex flex-row space-x-4">
-                  <div className="text-light_blue">Date Delivered:</div>
-                  <div className="text-black inline-block">{quoteHistory.deliveryDate || "N/A"}</div>
-                </div>
-                <div className="flex flex-row space-x-8">
-                  <div className="text-light_blue">Address:</div>
-                  <div className="text-black inline-block px-7">{quoteHistory.address1 || "N/A"}</div>
-                </div>
-                <div className="flex flex-row space-x-10">
-                  <div className="text-black inline-block px-28">{quoteHistory.address2 || ""}</div>
-                </div>
-                <div className="flex flex-row space-x-10">
-                  <div className="text-black inline-block px-28">{(quoteHistory.city || quoteHistory.state || quoteHistory.zipCode) ? (quoteHistory.city + ", " + quoteHistory.state + ", " + quoteHistory.zipCode) : ""}</div>
-                </div>
-              </div>
-              <div className>
-                <Link href="/QuoteHistory"><button className="px-6 py-2 mt-4 text-beige bg-light_blue rounded-lg hover:bg-light_blue/75 hover:text-beige">View Full<br></br>Quote History</button></Link>
-              </div>
+        <main className="flex flex-row justify-center items-center mx-6">
+          {/* left */}
+          <div className="flex justify-center items-center w-3/5 my-6 h-full">
+            <div className="flex w-full justify-center items-center bg-white px-6 py-6 mx-6 my-6 h-full shadow-lg">
+              <h1 className="text-2xl font-bold"> What is the Fuel Quoter?</h1>
             </div>
           </div>
-
-          <div className="flex flex-row">
-            <div className="px-8 py-6 text-left w-1/4 bg-white shadow-lg mx-auto">
-              <h3 className="text-2xl font-bold">PROFILE OVERVIEW</h3>
-                <div className="mt-4 flex flex-row space-x-12">
-                  <div className="text-light_blue">Name:</div>
-                  <div className="text-black inline-block px-1">{userData.firstName + " " + userData.lastName}</div>
+          {/* right half */}
+          <div className="flex flex-col justify-center items-center w-2/5 mx-6 my-6 h-full">
+            {/* top right */}
+            <div className="flex flex-col w-full justify-center items-center bg-white px-6 py-6 mb-6 mx-6 h-full shadow-lg">
+              <h1 className="text-xl font-bold"> Profile Overview</h1>
+              <div className="w-full text-left">
+                <div className="mt-2 flex flex-row w-full">
+                  <div className="w-1/5"></div>
+                  <div className="text-light_blue w-1/5">Name:</div>
+                  <div className="text-black items-center justify-center px-2 w-3/5"> {userData.firstName + " " + userData.lastName} </div>
                 </div>
-                <div className="flex flex-row space-x-9">
-                  <div className="text-light_blue">Address:</div>
-                  <div className="text-black inline-block">{userData.address1}</div>
-                </div>
-                <div className="flex flex-row space-x-10">
-                  <div className="text-black inline-block pl-24">{userData.address2}</div>
-                </div>
-                <div className="flex flex-row space-x-10">
-                  <div className="text-black inline-block pl-24">{userData.city + ", " + userData.state + ", " + userData.zipCode}</div>
+                <div className="flex flex-row w-full mt-1">
+                  <div className="w-1/5"></div>
+                  <div className="text-light_blue w-1/5">Address:</div>
+                  <div className=" flex-col flex w-3/5">
+                    <div className="text-black items-center justify-center px-2"> {userData.address1} </div>
+                    <div className="text-black items-center justify-center px-2"> {userData.address2} </div>
+                    <div className="text-black items-center justify-center px-2">
+                      {userData.city + ", " + userData.state + ", " + userData.zipCode}
+                    </div>
+                  </div>
                 </div>
                 <div className="flex-grow flex justify-center items-center">
-                  <Link href="/ProfilePage"><button className="px-6 py-2 mt-4 text-beige bg-light_blue rounded-lg hover:bg-light_blue/75 hover:text-beige">View / Edit Profile</button></Link>
+                  <Link href="/ProfilePage">
+                    <button className="px-6 py-2 mt-4 text-beige bg-light_blue rounded-md hover:bg-light_blue/75 hover:text-beige">
+                      View / Edit Profile
+                    </button>
+                  </Link>
                 </div>
+              </div>
             </div>
 
-            <div className="px-8 py-6 text-left w-3/5 bg-white shadow-lg m-auto">
-              <h3 className="text-2xl font-bold">WHAT IS THE FUEL QUOTER?</h3>
-                <div className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do <div className="text-light_blue inline-block">eiusmod</div> tempor incididunt 
-                    ut labore et dolore magna aliqua. Ut enim ad minim quis nostrud exercitation <div className="text-light_blue inline-block">ullamco</div> laboris 
-                    nisi ut aliquip ex ea commodo consequat. Duis aute <div className="text-light_blue inline-block">irure</div> dolor in reprehenderit in voluptate 
-                    velit <div className="text-light_blue inline-block">esse</div> dolore eu fugiat nulla pariatur. Excepteur sint occaecat <div className="text-light_blue inline-block">cupidatat</div> non 
-                    proident, sunt in culpa qui officia deserunt.
+            {/* bottom right */}
+            <div className="flex flex-col w-full justify-center items-center bg-white px-6 py-4 mt-6 mx-6 h-full shadow-lg">
+              <h1 className="text-xl font-bold mt-4"> Your Last Quote </h1>
+              <div className="w-full text-left">
+                <div className="mt-2 flex flex-row w-full items-center justify-center">
+                  <div className="w-[5%]"></div>
+                  <span className="text-light_blue w-[35%] px-2">Delivery Date:</span>
+                  <span className="text-black items-center justify-center px-2 w-[60%]"> {quoteHistory.deliveryDate} </span>
                 </div>
-                <div className="flex flex-row space-x-10 justify-center">
-                  <Link href="/QuoteForm"><button className="px-6 py-2 mt-4 text-beige bg-light_blue rounded-lg hover:bg-light_blue/75 hover:text-beige">Go to Fuel Quote Form</button></Link>
+                <div className="flex flex-row w-full items-center justify-center mt-1">
+                  <div className="w-[5%]"></div>
+                  <span className="text-light_blue w-[35%] px-2"> Address:</span>
+                  <span className="text-black items-center justify-center px-2 w-[60%]">
+                      {userData.city + ", " + userData.state + ", " + userData.zipCode}
+                  </span>
                 </div>
+                <div className="flex flex-row w-full items-center justify-center mt-1">
+                  <div className="w-[5%]"></div>
+                  <span className="text-light_blue w-[35%] px-2"> Gallons Requested:</span>
+                  <div className="text-black items-center justify-center px-2 w-[60%]"> {quoteHistory.gallonsRequested} </div>
+                </div>
+                <div className="flex flex-row w-full items-center justify-center mt-1">
+                  <div className="w-[5%]"></div>
+                  <span className="text-light_blue w-[35%] px-2"> Price / Gallon:</span>
+                  <div className="text-black items-center justify-center px-2 w-[60%]"> ${quoteHistory.pricePerGallon} </div>
+                </div>
+                <div className="flex flex-row w-full items-center justify-center mt-1">
+                  <div className="w-[5%]"></div>
+                  <span className="text-light_blue w-[35%] px-2"> Total Cost: </span>
+                  <div className="text-black items-center justify-center px-2 w-[60%]"> ${quoteHistory.totalAmountDue} </div>
+                </div>
+
+                <div className="flex-grow flex justify-center items-center">
+                  <Link href="/QuoteHistory">
+                    <button className="px-6 py-2 mt-4 mb-4 text-beige bg-light_blue rounded-md hover:bg-light_blue/75 hover:text-beige">
+                      View Quote History
+                    </button>
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-
+          
+        </main>
 
         {/* FOOTER */}
-        <Footer/>
-
+        <Footer />
       </div>
-      </div>
-      
-    );
-  }
+    </div>
+  );
+}
