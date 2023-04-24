ca77d500331534bdb7ea336a5ae8dec42ad00f0a
Wahab Javed <wahab.javed@live.com>
2023-04-24 00:01:52 -0500

    fixed pricingmodule test that broke slightly

--- tests/PricingModule.test.js
+++ tests/PricingModule.test.js
@@ -78,8 +78,8 @@ describe("GET /api/PricingModule", () => {
 
         expect(res._getStatusCode()).toBe(200);
         expect(res._getJSONData()).toEqual({
-            pricePerGallon: 1.71,
-            totalAmountDue: 1.71 * req.query.gallonsRequested,
+            pricePerGallon: 1.695,
+            totalAmountDue: 1.695 * req.query.gallonsRequested,
         });
         expect(History.findOne).toHaveBeenCalledWith({ email: session.user.email });
         expect(Profile.findOne).toHaveBeenCalledWith({ email: session.user.email });
@@ -119,8 +119,8 @@ describe("GET /api/PricingModule", () => {
 
         expect(res._getStatusCode()).toBe(200);
         expect(res._getJSONData()).toEqual({
-            pricePerGallon: 1.74,
-            totalAmountDue: 1.74 * req.query.gallonsRequested,
+            pricePerGallon: 1.755,
+            totalAmountDue: 1.755 * req.query.gallonsRequested,
         });
         expect(History.findOne).toHaveBeenCalledWith({ email: session.user.email });
         expect(Profile.findOne).toHaveBeenCalledWith({ email: session.user.email });
