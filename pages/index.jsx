2894fe9d52421e59e44bf31f5fba54b5bce8d6e7
Wahab Javed <wahab.javed@live.com>
2024-06-11 20:52:13 -0500

    sample test push

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import HomePage from './HomePage'
 import LoggedInHomePage from './LoggedInHomePage'
 import { useSession,signOut } from 'next-auth/react'
 import {redirect} from 'next/navigation'
+global.TextEncoder = global.TextEncoder || require('util').TextEncoder;
 export default function Home() {
   const { data: session, status } = useSession()
   return (
